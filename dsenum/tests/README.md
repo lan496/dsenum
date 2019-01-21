6dc034ff015862f15b0c19914c3c0818aac913f2
lan496 <kohei19950508@gmail.com>
2019-01-21 15:44:21 +0900

    test SNF and remained test case for superlattice

--- dsenum/tests/README.md
+++ dsenum/tests/README.md
@@ -1,6 +1,18 @@
+### Tests
+- `test_smith_normal_form.py`
+  - reproduce Table3 in [1]
 - 'test_superlattice.py'
-  check to reproduce https://oeis.org/A045790
-- unreproduced figure
-  - https://journals.aps.org/prb/abstract/10.1103/PhysRevB.77.224115
+  - reproduce https://oeis.org/A001001
+  - reproduce https://oeis.org/A045790
+  - reproduce Table4 in [1]
+- not tested figures
+  - [1]
     - fig2
     - fig3
+    - fig9
+    - fig10
+  - [2]
+
+### Refs.
+1. https://journals.aps.org/prb/abstract/10.1103/PhysRevB.77.224115
+1. https://journals.aps.org/prb/abstract/10.1103/PhysRevB.80.014120
--- dsenum/tests/test_superlattice.py
+++ dsenum/tests/test_superlattice.py
@@ -4,13 +4,68 @@ import numpy as np
 from pymatgen.core import Structure, Lattice
 from pymatgen.symmetry.analyzer import SpacegroupAnalyzer
 
-from derivative.superlattice import (
+from dsenum.superlattice import (
     generate_all_superlattices,
     reduce_HNF_list_by_parent_lattice_symmetry,
 )
+from dsenum.utils import get_lattice, get_symmetry_operations
 
 
-class TestDerivativeStructureEnumeration(unittest.TestCase):
+class TestSuperlattice(unittest.TestCase):
+
+    def test_generate_all_superlattices(self):
+        # https://oeis.org/A001001
+        num_expected = [1, 7, 13, 35, 31, 91, 57, 155, 130, 217,
+                        133, 455, 183, 399, 403, 651, 307, 910, 381, 1085,
+                        741, 931, 553, 2015, 806, 1281, 1210, 1995, 871, 2821,
+                        993, 2667, 1729, 2149, 1767, 4550, 1407, 2667, 2379, 4805,
+                        1723, 5187, 1893, 4655, 4030, 3871, 2257, 8463, 2850, 5642,
+                        3991, 6405, 2863]
+        max_index = len(num_expected)
+
+        for index, expected in zip(range(1, max_index + 1), num_expected):
+            list_HNF = generate_all_superlattices(index)
+            self.assertEqual(len(list_HNF), expected)
+
+    def test_reduce_HNF_list_by_parent_lattice_symmetry(self):
+        # confirm table 4
+        obj = {
+            'fcc': {
+                'structure': get_lattice('fcc'),
+                'num_expected': [1, 2, 3, 7, 5, 10, 7, 20, 14, 18]
+            },
+            'bcc': {
+                'structure': get_lattice('bcc'),
+                'num_expected': [1, 2, 3, 7, 5, 10, 7, 20, 14, 18]
+            },
+            'sc': {
+                'structure': get_lattice('sc'),
+                'num_expected': [1, 3, 3, 9, 5, 13, 7, 24, 14, 23]
+            },
+            'hex': {
+                'structure': get_lattice('hex'),
+                'num_expected': [1, 3, 5, 11, 7, 19, 11, 34, 23, 33]
+            },
+            'tetragonal': {
+                'structure': get_lattice('tet'),
+                'num_expected': [1, 5, 5, 17, 9, 29, 13, 51, 28, 53]
+            },
+            'hcp': {
+                'structure': get_lattice('hcp'),
+                'num_expected': [1, 3, 5, 11, 7, 19, 11, 34, 23, 33]
+            }
+        }
+
+        for name, dct in obj.items():
+            structure = dct['structure']
+            for index, expected in zip(range(1, len(dct['num_expected']) + 1), dct['num_expected']):
+                list_HNF = generate_all_superlattices(index)
+                rotations, _ = get_symmetry_operations(structure)
+
+                list_reduced_HNF = \
+                    reduce_HNF_list_by_parent_lattice_symmetry(list_HNF,
+                                                               rotations)
+                self.assertEqual(len(list_reduced_HNF), expected)
 
     def test_reduce_HNF_list_by_parent_lattice_symmetry_fcc_bcc(self):
         # https://oeis.org/A045790
@@ -38,8 +93,7 @@ class TestDerivativeStructureEnumeration(unittest.TestCase):
 
                 list_reduced_HNF = \
                     reduce_HNF_list_by_parent_lattice_symmetry(list_HNF,
-                                                               list_rotation_matrix,
-                                                               n_jobs=-1)
+                                                               list_rotation_matrix)
                 self.assertEqual(len(list_reduced_HNF), expected)
 
     def get_face_centered_cubic(self):
