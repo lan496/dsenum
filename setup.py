de3a18eb50be1f13aaeb29e537b1730d2616b410
lan496 <kohei19950508@gmail.com>
2019-06-05 16:41:38 +0900

    packaging with cython

--- setup.py
+++ setup.py
@@ -1,4 +1,12 @@
-from setuptools import setup
+from setuptools import setup, Extension
+from Cython.Distutils import build_ext
 
+ext_modules = [
+    Extension('dsenum.core', sources=['dsenum/core.pyx'])
+]
 
-setup()
+setup(
+    packages=['dsenum'],
+    ext_modules=ext_modules,
+    cmdclass={'build_ext': build_ext}
+)
