47b78eadb72ce48cf96382c9da01ead7ce2773aa
lan496 <kohei19950508@gmail.com>
2020-08-13 20:06:27 +0900

    simplify SnO1-x example

--- examples/Sn_oxide.py
+++ examples/Sn_oxide.py
@@ -1,63 +1,74 @@
 import os
 
-from pymatgen.io.cif import CifParser
-from pymatgen.core import Specie, DummySpecie
+import numpy as np
+from pymatgen.core import Lattice, Structure, Specie, DummySpecie
 
 from dsenum import StructureEnumerator
 from dsenum.utils import write_cif
 
 
-def get_structure(filename):
-    cp = CifParser(filename)
-    structure = cp.get_structures(primitive=True)[0]
+def get_rutile_structure():
+    # rutile structure taken from mp-856
+    a = 4.832
+    c = 3.243
+    x_4f = 0.3066
+
+    lattice = Lattice.from_parameters(a, a, c, 90, 90, 90)
+    species = ["Sn", "Sn", "O", "O", "O", "O"]
+    # fmt: off
+    frac_coords = np.array([
+        [0, 0, 0],                      # Sn(2a)
+        [0.5, 0.5, 0.5],                # Sn(2a)
+        [x_4f, x_4f, 0],                # O(4f)
+        [1 - x_4f, 1 - x_4f, 0],        # O(4f)
+        [0.5 - x_4f, 0.5 + x_4f, 0.5],  # O(4f)
+        [0.5 + x_4f, 0.5 - x_4f, 0.5],  # O(4f)
+    ])
+    # fmt: on
+    structure = Structure(lattice, species, frac_coords)
     return structure
 
 
 if __name__ == "__main__":
-    rutile = get_structure(
-        os.path.join(
-            os.path.dirname(os.path.abspath(__file__)), "TiO2_mp-2657_conventional_standard.cif"
-        )
-    )
-    index = 5
-    # mapping_color_species = [DummySpecie('X', 0), Specie('O', -2), Specie('Sn', +2), Specie('Sn', +4)]
+    # enumerate rutile-like SnO1-x derivative structures
+    rutile = get_rutile_structure()
+
+    max_index = 3
+
     mapping_color_species = [DummySpecie("X"), Specie("O"), Specie("Sn")]
-    num_type = len(mapping_color_species)
-    """
-    base_site_constraints = [[2, 3],  # 2a
-                             [2, 3],  # 2a
-                             [0, 1],  # 4f
-                             [0, 1],  # 4f
-                             [0, 1],  # 4f
-                             [0, 1]]  # 4f
-    """
+    num_types = len(mapping_color_species)
+    composition_constraints = None
+    # fmt: off
     base_site_constraints = [
         [2],  # 2a
         [2],  # 2a
         [0, 1],  # 4f
         [0, 1],  # 4f
         [0, 1],  # 4f
-        [0, 1],
-    ]  # 4f
+        [0, 1],  # 4f
+    ]
+    # fmt: on
 
-    se = StructureEnumerator(
-        rutile,
-        index,
-        num_type,
-        mapping_color_species,
-        base_site_constraints=base_site_constraints,
-        color_exchange=False,
-        leave_superperiodic=False,
-        use_all_colors=False,
-    )
-    list_dstructs = se.generate()
-
-    name = "SnOx_index={}".format(index)
-    dirname = os.path.join(os.path.dirname(os.path.abspath(__file__)), name)
+    dirname = os.path.join(os.path.dirname(os.path.abspath(__file__)), "SnO1-x")
     os.makedirs(dirname, exist_ok=True)
-    for i, dstruct in enumerate(list_dstructs):
-        # remove void
-        dstruct.remove_species([mapping_color_species[0]])
 
-        filename = os.path.join(dirname, "SnOx_index={}_{}.cif".format(index, i))
-        write_cif(filename, dstruct, refine_cell=True)
+    for index in range(1, max_index + 1):
+        se = StructureEnumerator(
+            rutile,
+            index,
+            num_types,
+            mapping_color_species=mapping_color_species,
+            composition_constraints=composition_constraints,
+            base_site_constraints=base_site_constraints,
+            color_exchange=False,
+            leave_superperiodic=False,
+            use_all_colors=False,
+        )
+        list_dstructs = se.generate()
+
+        for i, dstruct in enumerate(list_dstructs):
+            # remove void
+            dstruct.remove_species([mapping_color_species[0]])
+
+            filename = os.path.join(dirname, f"SnO1-x_{index}_{i}.cif")
+            write_cif(filename, dstruct, refine_cell=True)
