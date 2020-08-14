dcad3959dfe0b46a759255e530126a3890e6bf00
lan496 <kohei19950508@gmail.com>
2020-08-14 16:47:58 +0900

    get version

--- dsenum/__init__.py
+++ dsenum/__init__.py
@@ -1,4 +1,18 @@
 # setup.py reflects the below version string.
 __version__ = "0.3.2"
 
+import os
+import subprocess
+
 from dsenum.enumerate import StructureEnumerator
+
+
+def get_git_commit_hash() -> str:
+    cwd = os.path.dirname(os.path.abspath(__file__))
+    out = subprocess.check_output(["git", "rev-parse", "--short", "HEAD"], cwd=cwd)
+    return out.strip().decode("ascii")
+
+
+def get_version() -> str:
+    out = __version__ + "+" + get_git_commit_hash()
+    return out
