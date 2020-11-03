d3fb9589a73cca5fea31b61722e243fb35aa0944
lan496 <kohei19950508@gmail.com>
2020-11-03 22:05:30 +0900

    binary derivative structure

--- src/structure_enumeration.hpp
+++ src/structure_enumeration.hpp
@@ -16,34 +16,76 @@
 namespace pyzdd {
 
 namespace derivative_structure {
-    /// @brief encode (site, specie)
-    int encode_site_with_specie(int site, int specie, int num_types) {
-        return site * num_types + specie;
-    }
 
-    /// @brief return (site, specie)
-    std::pair<int, int> decode_to_site_and_specie(int idx, int num_types) {
-        return std::make_pair(idx / num_types, idx % num_types);
-    }
-
-    std::vector<permutation::Permutation> augment_permutations(const std::vector<permutation::Permutation>& perms, int num_sites, int num_types) {
-        std::vector<permutation::Permutation> augmented;
-        if (num_types == 2) {
-            // binary system
-            augmented = perms;
-        } else {
-            augmented.reserve(perms.size());
-            for (const auto& perm: perms) {
-                std::vector<permutation::Element> sigma(num_sites * num_types);
-                for (size_t site = 0; site < num_sites; ++site) {
-                    for (size_t specie = 0; specie < num_types; ++specie) {
-                        sigma[encode_site_with_specie(perm.permute(site), specie, num_types)] = encode_site_with_specie(site, specie, num_types);
-                    }
-                }
-                augmented.emplace_back(permutation::Permutation(sigma));
-            }
+    class SiteSpecieConverter {
+        const size_t num_sites_;
+        const size_t num_types_;
+        const size_t num_variables_;
+    public:
+        SiteSpecieConverter() = delete;
+        SiteSpecieConverter(int num_sites, int num_types) :
+            num_sites_(static_cast<size_t>(num_sites)),
+            num_types_(static_cast<size_t>(num_types)),
+            num_variables_(num_sites * num_types)
+        {
+            assert(num_types_ >= 2);
         }
-        return augmented;
+
+        /// @brief encode (site, specie)
+        permutation::Element encode(permutation::Element site, int specie) const {
+            return site * num_types_ + specie;
+        }
+
+        /// @brief return (site, specie)
+        std::pair<permutation::Element, int> decode(permutation::Element idx) const {
+            return std::make_pair(idx / num_types_, idx % num_types_);
+        }
+
+        permutation::Permutation augment_permutation(const permutation::Permutation& perm) const {
+            std::vector<permutation::Element> sigma(num_variables_);
+            for (size_t site = 0; site < num_sites_; ++site) {
+                for (size_t specie = 0; specie < num_types_; ++specie) {
+                    sigma[encode(perm.permute(site), specie)] = encode(site, specie);
+                }
+            }
+            auto ret = permutation::Permutation(sigma);
+            return ret;
+        }
+    };
+
+    /// @brief enumerate binary (num_types == 2) derivative structures
+    void enumerate_binary_derivative_structures(
+        int num_sites,
+        int num_types,
+        const std::vector<permutation::Permutation>& automorphism,
+        tdzdd::DdStructure<2>& dd
+    ) {
+        int num_variables = num_sites;
+
+        // ==== prepare specs ====
+        // spec for isomorphism elimination
+        // sort permutations by max frontier sizes
+        std::vector<permutation::Permutation> sorted_automorphism(automorphism);
+        std::sort(sorted_automorphism.begin(), sorted_automorphism.end(),
+                  [](const permutation::Permutation& lhs, const permutation::Permutation& rhs) {
+                      auto size_lhs = permutation::PermutationFrontierManager(lhs).get_max_frontier_size();
+                      auto size_rhs = permutation::PermutationFrontierManager(rhs).get_max_frontier_size();
+                      return size_lhs < size_rhs;
+                  });
+        std::vector<permutation::isomorphism::IsomorphismElimination> aut_specs;
+        aut_specs.reserve(sorted_automorphism.size());
+        for (const auto& perm: sorted_automorphism) {
+            permutation::PermutationFrontierManager pfm(perm);
+            permutation::isomorphism::IsomorphismElimination spec(pfm);
+            aut_specs.emplace_back(spec);
+        }
+
+        // ==== construct DD ====
+        dd = universe::Universe(num_variables);
+        for (const auto& spec: aut_specs) {
+            dd.zddSubset(spec);
+            dd.zddReduce();
+        }
     }
 
     /// @brief enumerate DD for nonequivalent derivative structures
@@ -65,22 +107,35 @@ namespace derivative_structure {
         int num_sites,
         int num_types,
         const std::vector<permutation::Permutation>& automorphism,
-        const std::vector<permutation::Permutation>& translations,
-        const std::vector<std::pair<std::vector<int>, int>>& composition_constraints,
-        const std::vector<std::vector<int>>& site_constraints,
-        bool remove_superperiodic,
-        bool remove_incomplete,
         tdzdd::DdStructure<2>& dd
     ) {
         // sanity check
         assert(num_sites >= 1);
         assert(num_types >= 2);
         for (const auto& perm: automorphism) {
-            if (perm.get_size() != num_sites) {
+            if (perm.get_size() != static_cast<size_t>(num_sites)) {
                 std::cerr << "The number of elements of permutation should be num_sites." << std::endl;
                 exit(1);
             }
         }
+
+        if (num_types == 2) {
+            enumerate_binary_derivative_structures(num_sites, num_types, automorphism, dd);
+        } else {
+            // TODO
+            assert(false);
+        }
+    }
+
+    /*
+    void enumerate_derivative_structures(
+        const std::vector<permutation::Permutation>& translations,
+        const std::vector<std::pair<std::vector<int>, int>>& composition_constraints,
+        // const std::vector<std::vector<permutation::Element>>& site_constraints,
+        bool remove_superperiodic,
+        bool remove_incomplete,
+    ) {
+
         for (const auto& perm: translations) {
             if (perm.get_size() != num_sites) {
                 std::cerr << "The number of elements of permutation should be num_sites." << std::endl;
@@ -119,39 +174,153 @@ namespace derivative_structure {
         }
 
         // one-hot encoding for num_types >= 3
-        bool is_binary_system = (num_types == 2);
-        int num_variables = is_binary_system ? num_sites : (num_sites * num_types);
-
-        auto automorphism_enc = augment_permutations(automorphism, num_sites, num_types);
-        auto translations_enc = augment_permutations(translations, num_sites, num_types);
-
+        int num_variables = (num_types == 2) ? num_sites : (num_sites * num_types);
+        std::vector<permutation::Permutation> automorphism_enc;
+        std::vector<permutation::Permutation> translations_enc;
         std::vector<std::pair<std::vector<int>, int>> composition_constraints_enc;
-        if (!composition_constraints.empty()) {
-            if (is_binary_system) {
-                composition_constraints_enc = composition_constraints;
-            } else {
+        // std::vector<std::vector<permutation::Element>> prohibited_species_enc;
+        if (num_types == 2) {
+            automorphism_enc = automorphism;
+            translations_enc = translations;
+            composition_constraints_enc = composition_constraints;
+
+            if (!site_constraints.empty()) {
+                prohibited_species_enc.reserve(site_constraints.size());
+                for (permutation::Element site = 0; site < num_sites; ++site) {
+                    std::set<int> allow;
+                    for (auto specie: site_constraints[site]) {
+                        allow.insert(specie);
+                    }
+
+                    std::vector<int> prohibited;
+                    for (int specie = 0; specie < num_types; ++specie) {
+                        if (allow.find(specie) == allow.end()) {
+                            prohibited.emplace_back(specie);
+                        }
+                    }
+                    prohibited_species_enc.emplace_back(prohibited);
+                }
+            }
+        } else {
+            auto converter = SiteSpecieConverter(num_sites, num_types);
+
+            automorphism_enc.reserve(automorphism.size());
+            for (const auto& perm: automorphism) {
+                automorphism_enc.emplace_back(converter.augment_permutation(perm));
+            }
+
+            translations_enc.reserve(translations.size());
+            for (const auto& perm: translations) {
+                translations_enc.emplace_back(converter.augment_permutation(perm));
+            }
+
+            if (!composition_constraints.empty()) {
                 composition_constraints_enc.reserve(composition_constraints.size());
-                for (int specie = 0; specie < num_types; ++num_types) {
-                    // TODO
+                for (int specie = 0; specie < num_types; ++specie) {
+                    const auto& group = composition_constraints[specie].first;
+                    int target_number = composition_constraints[specie].second;
+                    std::vector<int> augmented_group;
+                    augmented_group.reserve(group.size());
+                    for (auto site: group) {
+                        augmented_group.emplace_back(converter.encode(site, specie));
+                    }
+                    composition_constraints_enc.emplace_back(std::make_pair(augmented_group, target_number));
+                }
+            }
+
+            if (!site_constraints.empty()) {
+                prohibited_species_enc.reserve(site_constraints.size());
+                for (permutation::Element site = 0; site < num_sites; ++site) {
+                    std::set<int> allow;
+                    for (auto specie: site_constraints[site]) {
+                        allow.insert(specie);
+                    }
+
+                    std::vector<int> prohibited;
+                    for (int specie = 0; specie < num_types; ++specie) {
+                        if (allow.find(specie) == allow.end()) {
+                            prohibited.emplace_back(specie);
+                        }
+                    }
+
+                    std::vector<permutation::Element> augmented;
+                    augmented.reserve(site_constraints[site].size());
+                    for (auto specie: site_constraints[site]) {
+                        augmented.emplace_back(converter.encode(site, specie));
+                    }
+                    site_constraints_enc.emplace_back(augmented);
                 }
             }
         }
 
-        // prepare specs
-        std::vector<pyzdd::choice::Choice> choice_specs;
-        if (!composition_constraints.empty()) {
-            // TODO
-            choice_specs.reserve(num_types);
-            for (const auto& pair: composition_constraints) {
+
+        // spec for superperiodic elimination
+        std::vector<permutation::superperiodic::SuperperiodicElimination> sp_specs;
+        sp_specs.reserve(translations_enc.size());
+        for (const auto& perm: translations_enc) {
+            permutation::PermutationFrontierManager pfm(perm);
+            permutation::superperiodic::SuperperiodicElimination spec(pfm);
+            sp_specs.emplace_back(spec);
+        }
+
+        // spec for composition constraints
+        std::vector<choice::Choice> composition_specs;
+        if (!composition_constraints_enc.empty()) {
+            composition_specs.reserve(num_types);
+            for (const auto& pair: composition_constraints_enc) {
                 const auto& group = pair.first;
                 int k = pair.second;
+                // take exactly k elements from group (do not care element not in group)
+                choice::Choice spec(num_variables, k, group, false);
+                composition_specs.emplace_back(spec);
+            }
+        }
+
+        // spec for site constraints
+        std::vector<pyzdd::choice::Choice> sites_specs;
+        if (!site_constraints_enc.empty()) {
+            sites_specs.reserve(num_variables);
+            for (permutation::Element site = 0; site < num_sites; ++site) {
+                std::set<int> allow;
+                for (auto e: site_constraints_enc[site]) {
+
+                }
+            }
+
+            for (const auto& group: site_constraints_enc) {
                 // take exactly k elements from group (do not care element not in group)
                 pyzdd::choice::Choice spec(num_variables, k, group, false);
                 choice_specs.emplace_back(spec);
             }
         }
 
-        tdzdd::DdStructure<2> dd = universe::Universe(num_variables);
+        // spec for one-hot encoding
+        std::vector<choice::
+
+    }
+    */
+
+    std::vector<permutation::Element> convert_to_labeling(
+        tdzdd::DdStructure<2>::const_iterator const &itr,
+        int num_sites,
+        int num_types)
+    {
+        int num_variables = (num_types == 2) ? num_sites : (num_sites * num_types);
+        std::vector<permutation::Element> labeling(num_variables, 0);
+
+        if (num_types == 2) {
+            for (auto level: *itr) {
+                labeling[num_variables - level] = 1;
+            }
+        } else {
+            auto converter = SiteSpecieConverter(num_sites, num_types);
+            for (auto level: *itr) {
+                auto site_specie = converter.decode(num_variables - level);
+                labeling[site_specie.first] = site_specie.second;
+            }
+        }
+
+        return labeling;
     }
 
 } // namespace derivative_structure
---
+++ src/test/test_structure_enumeration.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <unordered_set>
+#include <cassert>
+
+#include <tdzdd/DdSpec.hpp>
+// #include <tdzdd/DdStructure.hpp>
+
+#include "type.hpp"
+#include "permutation.hpp"
+#include "structure_enumeration.hpp"
+
+using namespace pyzdd;
+using namespace pyzdd::permutation;
+using namespace pyzdd::derivative_structure;
+
+// https://stackoverflow.com/questions/29855908/c-unordered-set-of-vectors
+template<typename T>
+struct VectorHash {
+    size_t operator()(const std::vector<T>& v) const {
+        std::hash<T> hasher;
+        size_t seed = 0;
+        for (auto i : v) {
+            seed ^= hasher(i) + 0x9e3779b9 + (seed<<6) + (seed>>2);
+        }
+        return seed;
+    }
+};
+
+int main() {
+    // reproduce Fig.2
+    int num_sites = 4;
+    int num_types = 2;
+    auto c4 = Permutation(std::vector<Element>{1, 2, 3, 0});
+    auto m = Permutation(std::vector<Element>{3, 2, 1, 0});
+    auto automorphism = generate_group(std::vector<Permutation>{c4, m});
+    assert(automorphism.size() == 8);
+    tdzdd::DdStructure<2> dd;
+
+    enumerate_derivative_structures(num_sites, num_types, automorphism, dd);
+
+    std::string cardinality_expect = "6";
+    auto cardinality_actual = dd.zddCardinality();
+    assert(cardinality_actual == cardinality_expect);
+
+    std::vector<std::vector<Element>> expect = {
+        {0, 0, 0, 0},
+        {1, 0, 0, 0},
+        {1, 0, 1, 0},
+        {1, 1, 0, 0},
+        {1, 1, 1, 0},
+        {1, 1, 1, 1},
+    };
+    std::unordered_set<std::vector<Element>, VectorHash<Element>> uset_expect;
+    for (auto labeling: expect) {
+        uset_expect.insert(labeling);
+    }
+    std::unordered_set<std::vector<Element>, VectorHash<Element>> uset_actual;
+    for (auto itr = dd.begin(), end = dd.end(); itr != end; ++itr) {
+        auto labeling = convert_to_labeling(itr, num_sites, num_types);
+        uset_actual.insert(labeling);
+    }
+    assert(uset_actual == uset_expect);
+
+#ifdef _DEBUG
+    for (auto labeling: uset_actual) {
+        for (auto c: labeling) {
+            std::cerr << " " << c;
+        }
+        std::cerr << std::endl;
+    }
+#endif
+
+    return 0;
+}
